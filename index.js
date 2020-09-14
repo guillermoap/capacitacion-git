83e338cd2a0f3aea7ce9f07027050cd11ef216e7
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2020-09-14 01:18:08 -0300

    Add new endpoints

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-      res.send('Capacitación Git');
+  res.send('PIS 2020');
 });
+
+app.get('/version', (req, res) => {
+  res.send('1.0.0')
+});
+
+app.get('/name', (req, res) => {
+  res.send('SeptemberLabs')
+})'
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
